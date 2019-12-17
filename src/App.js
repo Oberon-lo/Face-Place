81ce7b35150df6aeac11c914b3dfe20fb48efcfd
nhloder <nhloder@gmail.com>
2019-12-17 14:53:29 -0700

    nodemailer-and-server-file-added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 function App() {
-  function sendIt(){
-    axios.post('/api/send')
-    .then(
-      alert('Email Sent!!!')
-    )
-  }
+
   return (
     <div className="App">
-      <button onClick = {()=> sendIt()}>Send Email</button>
+      welcome to the app
     </div>
   );
 }
