25c7f550c884dfbc570470d5d520cd47500e9a80
nhloder <nhloder@gmail.com>
2019-12-17 13:36:56 -0700

    nodemailer-sends-mail

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
+const aws = require("aws-sdk");
+const uuid = require('uuid/v4');
+const nodemailer = require('./controllers/nodemailer.js');
+
+const {SESSION_SECRET, PASSWORD, SERVER_PORT} = process.env;
+
+const app = express();
+// TOP LEVEL MIDDLEWARE \\
+app.use(express.json());
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  })
+);
+
+// massive(CONNECTION_STRING).then(db => {
+//   app.set("db", db);
+  // console.log("TAC-COM ONLINE");
+  app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} ducks marching on rome`));
+// });
+
+// NODEMAILER \\
+app.post('/api/send', nodemailer.nodemailer)
