1cdb0739c1be7889dc0f65369e508de8c4270ca3
WillHoll <willhollingsworth15@yahoo.com>
2019-12-19 18:33:41 -0700

    1/4 way through comment controller

--- server/controllers/comController.js
+++ server/controllers/comController.js
@@ -1,3 +1,39 @@
 module.exports = {
-  
+  getCom: async (req, res) => {
+    const db = req.app.get('db');
+    const { post_id } = req.params;
+    try {
+      const comNumber = await db.comment.get_com_count([post_id]);
+      const comments = await db.comment.get_all_com([post_id]);
+      res.status(200).send({ comNumber, comments })
+    } catch (err) {
+      return res.send(err);
+    }
+  },
+  getComImg: async (req, res) => {
+    const db = req.app.get('db');
+    const { com_id } = req.params;
+    try {
+      const imageArr = await db.comment.get_com_img([com_id]);
+      res.status(200).send(imageArr);
+    } catch (err) {
+      return res.send(err);
+    };
+  },
+  makeCom: async (req, res) => {
+    const db = req.app.get('db');
+
+  },
+  editCom: async (req, res) => {
+    const db = req.app.get('db');
+
+  },
+  deleteComImg: async (req, res) => {
+    const db = req.app.get('db');
+
+  },
+  deleteCom: async (req, res) => {
+    const db = req.app.get('db');
+
+  },
 };
