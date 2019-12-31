07293c148ce44a44d819fd4b2b180f625700f1f2
WillHoll <willhollingsworth15@yahoo.com>
2019-12-31 15:41:10 -0700

    home complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import Post from './../PostDisplay/Post';
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import {retrievePosts, getSession} from '../../ducks/reducer';
 
-const Home = () => {
+
+const Home = (props) => {
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        postGetter();
+    }, [props.user_id])
+    
+    function postGetter() {
+        retrievePosts(props.user_id, offset);
+        setOffset(offset + 5);
+    }
+    
+
     return (
         <div>
             HOME
-            <Post/>
+            {
+                props.postArr.map((post, i) => (
+                    <div key={i} className="post-container">
+                        <Post post={post} />
+                    </div>
+                ))
+            }
         </div>
     );
 };
 
-export default Home;
+function mapStateToProps(reduxState) {
+    const { user_id, postArr } = reduxState;
+    return {
+        user_id,
+        postArr
+    }
+}
+
+export default connect(mapStateToProps, {retrievePosts, getSession})(Home);
