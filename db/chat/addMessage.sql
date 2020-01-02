0ce7edbdb9935b081f92a5910f013a21a2f6eacc
Shayla Paulsen <shayla.paulsen@gmail.com>
2020-01-02 15:43:18 -0700

    even more functionality wweeee

---
+++ db/chat/addMessage.sql
@@ -0,0 +1,5 @@
+INSERT INTO chat_message (chat_cont, post_time)
+VALUES (
+    ${chat_cont}, 
+    ${post_time}
+);
---
+++ db/chat/createUserChat.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_chat (user_id, chat_id)
+VALUES (${user_id}, ${chat_id});
